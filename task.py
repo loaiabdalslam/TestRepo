9bb3ec668b6ad3cb4148c4f022d8a1a00e76953f
Loaiabdalslam <32424891+loaiabdalslam@users.noreply.github.com>
2024-04-01 00:59:11 +0200

    Initial commit

---
+++ task.py
@@ -0,0 +1,5 @@
+Task 1: 
+Please implement a class using Object-Oriented Programming (OOP) in Python. The class should represent a vehicle and include attributes such as make, model, year, and color. Additionally, include methods to update the year of the vehicle and to display all the information about the vehicle.
+
+Task 2:
+Implement a stack data structure in C++. Create a class called Stack that has methods for push (to add elements to the stack), pop (to remove elements from the stack), and isEmpty (to check if the stack is empty). Test your implementation with a few sample elements being pushed and popped from the stack.
